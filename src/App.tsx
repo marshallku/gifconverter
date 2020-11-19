fc926741a793140934f61e48748a264b1ed77354
marshall-ku <marshall@kakao.com>
2020-11-19 23:44:09 +0900

    update file conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,139 @@
-import React, { useState, useEffect } from 'react';
-import { createFFmpeg, fetchFile, FFmpeg } from '@ffmpeg/ffmpeg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createFFmpeg, fetchFile, FFmpeg } from "@ffmpeg/ffmpeg";
+import "./App.css";
 
 const ffmpeg: FFmpeg = createFFmpeg({ log: true });
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  const [ready, setReady] = useState(false);
+    const [ready, setReady] = useState<boolean>(false);
+    const [input, setInput] = useState<{
+        file: File;
+        type: string;
+    }>();
+    const [output, setOutput] = useState<string>("");
 
-  const load = async () => {
-    await ffmpeg.load();
-    setReady(true);
-  };
+    const load = async () => {
+        await ffmpeg.load();
+        setReady(true);
+    };
 
-  useEffect(() => {
-    load();
-  }, []);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = event.target;
 
-  return <div className="App"></div>;
+        if (files !== null && files[0]) {
+            const file = files[0];
+            const { type } = file;
+
+            if (type === "image/gif" || type === "video/mp4") {
+                setInput({
+                    file: files[0],
+                    type,
+                });
+            }
+        }
+    };
+
+    const convertFile = async () => {
+        if (input === undefined) return;
+        const { file, type } = input;
+
+        if (type === "image/gif") {
+            // convert gif to mp4
+
+            ffmpeg.FS("writeFile", "input.gif", await fetchFile(file));
+            await ffmpeg.run(
+                "-f",
+                "gif",
+                "-i",
+                "input.gif",
+                "-movflags",
+                "+faststart",
+                "-pix_fmt",
+                "yuv420p",
+                "-vf",
+                "scale=trunc(iw/2)*2:trunc(ih/2)*2",
+                "output.mp4"
+            );
+
+            const data = ffmpeg.FS("readFile", "output.mp4");
+
+            const url = URL.createObjectURL(
+                new Blob([data.buffer], { type: "video/mp4" })
+            );
+
+            setOutput(url);
+        } else {
+            // convert mp4 to gif
+
+            //  ffmpeg -i input.mp4 -vf "fps=10,scale=320:-1:flags=lanczos" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize output.gif
+            // -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse"
+            ffmpeg.FS("writeFile", "input.mp4", await fetchFile(file));
+            await ffmpeg.run(
+                "-f",
+                "mp4",
+                "-i",
+                "input.mp4",
+                "-t",
+                "3",
+                "-ss",
+                "0.5",
+                "-filter_complex",
+                "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse",
+                "output.gif"
+            );
+
+            const data = ffmpeg.FS("readFile", "output.gif");
+
+            const url = URL.createObjectURL(
+                new Blob([data.buffer], { type: "image/gif" })
+            );
+
+            setOutput(url);
+        }
+    };
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    useEffect(() => {
+        convertFile();
+    }, [input]);
+
+    return (
+        <>
+            {ready ? (
+                <div>
+                    <input
+                        type="file"
+                        name=""
+                        id="file"
+                        hidden
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="file">UPLOAD</label>
+
+                    {input &&
+                        output &&
+                        (input.type !== "image/gif" ? (
+                            <img src={output} />
+                        ) : (
+                            <video
+                                src={output}
+                                autoPlay
+                                playsInline
+                                muted
+                                loop
+                            ></video>
+                        ))}
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </>
+    );
 }
 
 export default App;
