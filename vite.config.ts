b5495dbd752d7fbcb3c90c8c1be9f0a8f8261cf9
marshallku <marshall@kakao.com>
2022-06-14 14:02:35 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/gifconverter/",
     server: {
         headers: {
             "Cross-Origin-Opener-Policy": "same-origin",
