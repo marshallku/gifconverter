7b39143e59d75bcf47cc0d1faac0b25e4dfa6fc5
Marshall Ku <marshall@kakao.com>
2023-07-09 22:41:54 +0900

    Refactor DisplayOutput

--- src/@types/DisplayOutput.d.ts
+++ src/@types/DisplayOutput.d.ts
@@ -1,4 +1,15 @@
+type MediaProps = DetailedHTMLProps<
+    ImgHTMLAttributes<HTMLImageElement>,
+    | HTMLImageElement
+    | DetailedHTMLProps<VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>
+>;
+
 interface DisplayOutputProps {
     input: File;
     output: Output;
+}
+
+interface OutputProps<T extends MediaProps> extends DisplayOutputProps {
+    Input: T;
+    Output: T;
 }
